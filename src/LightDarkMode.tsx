0af19c4011ac753f4287e3c0e6e0052ce2325620
Kevin Burton <burtonator@gmail.com>
2021-07-29 10:06:26 -0600

    light dark mode test...

---
+++ src/LightDarkMode.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+export const App = () => {
+
+    return (
+        <Main/>
+    );
+
+}
+
+
+export const Main = () => {
+
+    return (
+        <div>
+            <Settings/>
+        </div>
+    );
+
+}
+
+
+
+export const Settings = () => {
+
+    const toggleMode = React.useCallback(() => {
+
+    }, []);
+
+    return (
+        <button onClick={toggleMode}>toggle light/dark mode</button>
+    );
+
+}
+
+
+
