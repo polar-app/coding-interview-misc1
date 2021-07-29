b5179e34a43a54555876c805e30f0da14c2940a6
Kevin Burton <burtonator@gmail.com>
2021-07-29 10:21:54 -0600

    RingBuffer...

---
+++ src/RingBuffer.ts
@@ -0,0 +1,33 @@
+/**
+ * Implement a class named ring buffer with fixed capacity such that
+ *
+ * constructor: takes the capacity for the ring buffer
+ *
+ * push: adds a value to the ring buffer.
+ * pop: removes the last value from the ring buffer or undefined if it's empty.
+ * peek: returns the current value of the most recent value added or undefined if none have been added
+ *
+ * If we have too many values (exceeding capacity) the oldest values are lost.
+ *
+ * The ordering of the push operations must be kept.
+ */
+class RingBuffer<T> {
+
+    constructor(capacity: number) {
+    }
+
+    public push(value: T) {
+
+    }
+
+    public peek(): T | undefined {
+        // not implemented
+        return undefined;
+    }
+
+    public pop(): T | undefined {
+        // not implemented
+        return undefined;
+    }
+
+}
---
+++ src/RingBufferTest.ts
@@ -0,0 +1,35 @@
+import {assert} from 'chai';
+
+describe("RingBuffer", function() {
+
+    it("basic", function () {
+
+        const rb = new RingBuffer<number>(3);
+
+        assert.isUndefined(rb.peek());
+
+        rb.push(1);
+        assert.equal(rb.peek(), 1);
+
+        assert.equal(rb.pop(), 1);
+
+        assert.isUndefined(rb.peek());
+
+    });
+
+    it("lost data", function() {
+
+        const rb = new RingBuffer<number>(3);
+        rb.push(1);
+        rb.push(2);
+        rb.push(3);
+        rb.push(4);
+
+        assert.equal(rb.pop(), 4);
+        assert.equal(rb.pop(), 3);
+        assert.equal(rb.pop(), 2);
+        assert.isUndefined(rb.pop());
+
+    });
+
+});
