5b8f02ae4a80a1824ee82ddfdb128bf6d081b45f
Kevin Burton <burtonator@gmail.com>
2021-12-13 13:15:28 -0700

    Updated dox.

--- src/LightDarkMode.tsx
+++ src/LightDarkMode.tsx
@@ -1,5 +1,19 @@
 import React from 'react';
 
+/**
+ *
+ * Build a simple app that allows the user to toggle light and dark mode as a react hook.
+ *
+ * Components will need a useMode() hook either 'light' or 'dark' so that they can change
+ * their internal CSS.
+ *
+ * There should also be a way to useModeToggler() which returns a function that can be used
+ * to toggle light or dark mode.
+ *
+ * The idea is that you have a way to globally mark the theme for the entire
+ * app, then a hook that can be used to change the theme.
+ *
+ */
 export const App = () => {
 
     return (
@@ -8,6 +22,11 @@ export const App = () => {
 
 }
 
+export type Theme = 'light' | 'dark';
+
+export type UseThemeToggler = (theme: Theme) => void;
+
+export type UseTheme = () => Theme;
 
 export const Main = () => {
 
--- src/TextMergeJoin.ts
+++ src/TextMergeJoin.ts
@@ -1,3 +1,15 @@
+/**
+ * In our app we have regions of text that may or not be contiguous.
+ *
+ * The text is given back as rectangles with x, y, width, and height properties.
+ *
+ * If the x, y, width, and height are close enough, we can assume they're the same word.
+ *
+ * Sometimes our rectangles are word fragments NOT the whole word so we need to join the words
+ * again to form entire sentences.
+ *
+ * The test data has examples of what these partial regions would look like.
+ */
 export namespace TextMergeJoin {
 
     export interface IPDFTextWord {
@@ -9,6 +21,9 @@ export namespace TextMergeJoin {
         readonly str: string;
     }
 
+    /**
+     *
+     */
     export function doMergeWords(data: ReadonlyArray<IPDFTextWord>): ReadonlyArray<IPDFTextWord> {
         return [];
     }
