182999e6419f39ccd32d23d0759e7ce5f7c5fa22
Kevin Burton <burtonator@gmail.com>
2021-12-16 13:27:17 -0700

    moving over to RTL tests...

--- src/UseLocalStorageTest.tsx
+++ src/UseLocalStorageTest.tsx
@@ -1,18 +1,21 @@
 import React from 'react'
-import {fireEvent, render, screen, waitFor} from '@testing-library/react'
-
+import {fireEvent, render, screen, waitFor} from '@testing-library/react';
+import {assert} from 'chai';
 import {useLocalStorageState} from "./UseLocalStorage";
 
 type ButtonCheckStr = 'off' | 'on';
 
+const LOCALSTORAGE_KEY = 'remember-password';
+
 export const RememberPassword = () => {
 
     // TODO: change this to use useLocalStorageState
-    const [value, setValue] = useLocalStorageState<ButtonCheckStr>('remember-password', 'off');
+    const [value, setValue] = useLocalStorageState<ButtonCheckStr>(LOCALSTORAGE_KEY, 'off');
 
     return (
         <input type="checkbox"
-               value={value}
+               data-testid="checkbox"
+               checked={value === 'on'}
                onChange={event => setValue(event.currentTarget.checked ? 'on' : 'off')}/>
     );
 
@@ -20,13 +23,43 @@ export const RememberPassword = () => {
 
 describe("UseLocalStorage", function() {
 
+    afterEach(() => {
+        localStorage.removeItem(LOCALSTORAGE_KEY);
+    });
+
     it("Test with no localStorage value", async () => {
 
         render(<RememberPassword/>)
 
-        // fireEvent.click(screen.getByText('Click Me'))
-        //
-        // await waitFor(() => screen.getByText("The user clicked the button!"))
+        const checkbox = screen.getByTestId('checkbox') as HTMLInputElement;
+
+        assert.isFalse(checkbox.checked);
+
+    });
+
+    it("Test with pre-existing localStorage value", async () => {
+
+        localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify('on'));
+
+        render(<RememberPassword/>)
+
+        const checkbox = screen.getByTestId('checkbox') as HTMLInputElement;
+
+        assert.isTrue(checkbox.checked);
+
+    });
+
+    it("No value in localStorage, click the button, then see if localStorage changed", async () => {
+
+        assert.isNull(localStorage.getItem(LOCALSTORAGE_KEY));
+
+        render(<RememberPassword/>)
+
+        const checkbox = screen.getByTestId('checkbox') as HTMLInputElement;
+
+        fireEvent.click(checkbox)
+
+        assert.equal(localStorage.getItem(LOCALSTORAGE_KEY), "\"on\"");
 
     });
 
